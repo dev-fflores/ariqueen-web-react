5f10cac11da9802593e01cbb8a9b1a799487c04d
fflores <ffloresrutti@gmail.com>
2024-03-02 08:40:54 -0500

    [feat]: Routes added to link a another pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,46 @@
 import './Header.css'
 import logoBBG from './../assets/logo_svg.svg';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import GamesPage from './GamesPage';
+import AboutUsPage from './AboutUsPage';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faLanguage } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <>
-      <header className='header'>
-        <img className='logo' src={logoBBG} alt="" srcset="" />
-      </header>
+      <Router>
+        <header className='header'>
+          <div className='header-logo'>
+            <img className='logo__img' src={logoBBG} alt="" srcSet="" />
+            <p className='logo__text'>Blood <br /> Bound Games</p>
+          </div>
+          <nav className='header-nav'>
+            <ul className='nav__list'>
+              <li className='nav__item'><Link to='/home' className='nav__link'>Home</Link></li>
+              <li className='nav__item'><Link to='/games' className='nav__link'>Games</Link></li>
+              <li className='nav__item'><Link to='/about-us' className='nav__link'>About Us</Link></li>
+              <li className='nav__item'><Link to='#' className='nav__link'>Contact Me</Link></li>
+            </ul>
+          </nav>
+
+          <div className='header-auth-buttons'>
+            {/* <FontAwesomeIcon className='header-auth-buttons__icon' icon={faLanguage} /> */}
+            <button className='header-auth-buttons__login auth__button'>Log In</button>
+            <button className='header-auth-buttons__signup auth__button auth__button--active'>Sign Up</button>
+          </div>
+        </header>
+
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/games" element={<GamesPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+
+
+      </Router>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from '../src/components/Header'
+import HomePage from '../src/components/HomePage'
 import './normalize.css'
 import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
